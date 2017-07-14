1d56a998de7fddace96344e66df829b0cffaec93
Autumn <autumncbullard@gmail.com>
2017-07-14 18:15:36 -0400

    Added JS solution for "Simple Pig Latin"

---
+++ js/simple-pig-latin.js
@@ -0,0 +1,21 @@
+// Simple Pig Latin
+
+/*
+Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+*/
+
+function pigIt(str){
+
+  str = str.split(' ');
+
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].split('');
+    str[i].push(str[i][0] + 'ay');
+    str[i].shift();
+    str[i] = str[i].join('')
+  }
+
+  return str.join(' ');
+}
+
+pigIt('Pig latin is cool');
